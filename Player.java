5b8f7bc03bfdbb078e400701c0f45f04706baf8c
ColTo <ctopper@uw.edu>
2018-01-11 14:32:28 -0800

    I cleaned up the Player file a little bit.

--- Player.java
+++ Player.java
@@ -5,28 +5,13 @@ import java.util.*;
 
 public class Player {
 
-    private static int holder = 0;
-
     //Holds all pth objects for robots that are currently enroute.
     private static List<Path> activePaths = new LinkedList<Path>(); 
 
     public static void main(String[] args) {
-        // MapLocation is a data structure you'll use a lot.
-        MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
-        System.out.println("loc: "+loc+", one step to the Northwest: "+loc.add(Direction.Northwest));
-        System.out.println("loc x: "+loc.getX());
-
-        // One slightly weird thing: some methods are currently static methods on a static class called bc.
-        // This will eventually be fixed :/
-        System.out.println("Opposite of " + Direction.North + ": " + bc.bcDirectionOpposite(Direction.North));
-
         // Connect to the manager, starting the game
         GameController gc = new GameController();
-
         Pathfinder pf = new Pathfinder();
-
-        // Direction is a normal java enum.
-        Direction[] directions = Direction.values();
 
         while (true) {
             System.out.println("Current round: "+gc.round());
@@ -35,21 +20,11 @@ public class Player {
             for (int i = 0; i < units.size(); i++) {
                 Unit unit = units.get(i);
 
-                activateRobot(unit);
+                activateUnit(unit);
 
                 moveRobots();
-
-                // Most methods on gc take unit IDs, instead of the unit objects themselves.
-                if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.Southeast) && (holder % 2) == 0) {
-                    gc.moveRobot(unit.id(), Direction.Southeast);
-                }
-
-                else if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.Northwest)) {
-                    gc.moveRobot(unit.id(), Direction.Northwest);
-                }
-
-                holder++;
             }
+
             // Submit the actions we've done, and wait for our next turn.
             gc.nextTurn();
         }
@@ -67,7 +42,7 @@ public class Player {
         }
     }
 
-    public static void activateRobot(Unit unit) {
+    public static void activateUnit(Unit unit) {
         UnitType type = unit.unitType()
         switch (type) { 
             case Factory: 
