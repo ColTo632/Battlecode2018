7d25facdc39bb43c10235d256d57518d5391065c
ColTo <ctopper@uw.edu>
2018-01-11 14:13:55 -0800

    I made a state machine to help organize our unit specific code.

--- Player.java
+++ Player.java
@@ -5,10 +5,10 @@ import java.util.*;
 
 public class Player {
 
-private static int holder = 0;
+    private static int holder = 0;
 
-//Holds all pth objects for robots that are currently enroute.
-private static List<Path> activePaths = new LinkedList<Path>(); 
+    //Holds all pth objects for robots that are currently enroute.
+    private static List<Path> activePaths = new LinkedList<Path>(); 
 
     public static void main(String[] args) {
         // MapLocation is a data structure you'll use a lot.
@@ -23,24 +23,21 @@ private static List<Path> activePaths = new LinkedList<Path>();
         // Connect to the manager, starting the game
         GameController gc = new GameController();
 
+        Pathfinder pf = new Pathfinder();
+
         // Direction is a normal java enum.
         Direction[] directions = Direction.values();
 
         while (true) {
             System.out.println("Current round: "+gc.round());
 
-            //This little loop marchs all of our robots with paths forward one step and removes them when their paths are completed.
-            Iterator<Path> it = activePaths.iterator();
-            while(it.hasNext()) {
-                Path path = it.next();
-                if (path.continuePath())
-                    it.remove();
-            }
-
-            // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
             VecUnit units = gc.myUnits();
             for (int i = 0; i < units.size(); i++) {
                 Unit unit = units.get(i);
+
+                activateRobot(unit);
+
+                moveRobots();
 
                 // Most methods on gc take unit IDs, instead of the unit objects themselves.
                 if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.Southeast) && (holder % 2) == 0) {
@@ -57,4 +54,72 @@ private static List<Path> activePaths = new LinkedList<Path>();
             gc.nextTurn();
         }
     }
+
+    // Progress all activate paths
+    public static void moveRobots() {
+    // This loop marchs all of our robots with paths forward one step and removes them from the paths list
+    // when they reach their destination.
+        Iterator<Path> it = activePaths.iterator();
+        while(it.hasNext()) {
+            Path path = it.next();
+            if (path.continuePath())
+                it.remove();
+        }
+    }
+
+    public static void activateRobot(Unit unit) {
+        UnitType type = unit.unitType()
+        switch (type) { 
+            case Factory: 
+                activateFactory(unit);
+                break;
+            case Healer:
+                activateHealer(unit);
+                break;
+            case Knight:
+                activateKnight(unit);
+                break;
+            case Mage:
+                activateMage(unit);
+                break;
+            case Ranger:
+                activateRanger(unit);
+                break;
+            case Rocket:
+                activateRocket(unit);
+                break;
+            case Worker:
+                activateWorker(unit);
+                break;
+        }
+    }
+
+    public static void activateFactory(Unit unit) {
+        return;
+    }
+
+    public static void activateHealer(Unit unit) {
+        return;
+    }
+
+    public static void activateKnight(Unit unit) {
+        return;
+    }
+
+    public static void activateMage(Unit unit) {
+        return;
+    }
+
+    public static void activateRanger(Unit unit) {
+        return;
+    }
+
+    public static void activateRocket(Unit unit) {
+        return;
+    }
+
+    public static void activateWorker(Unit unit) {
+        return;
+    }
+
 }
