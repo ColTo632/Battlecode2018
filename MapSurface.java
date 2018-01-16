969cfedcd10b6afb26b7726099f5d96ab9a9cd83
ANKH-MORPORK\Nathaniel <n_kintner@hotmail.com>
2018-01-15 19:51:41 -0800

    Written inverse-pathfinding for ranger exploration

--- MapSurface.java
+++ MapSurface.java
@@ -51,6 +51,21 @@ public class MapSurface {
 		//while there is a node in consideration
 	}
 	
+	public MapSurface(PlanetMap pmWorld, ArrayList<MapLocation> lstML) {
+		
+		System.out.println("Init MapSurface");
+		CenterX = -1;
+		CenterY = -1;
+		PUID = (x * 100) + y;
+		long pWidth = pmWorld.getWidth();
+		long pHeight = pmWorld.getHeight();
+		Planet p = pmWorld.getPlanet();
+		Surface = new short[(int)pWidth][(int)pHeight];
+		short distance = -6;
+		propagateMap(lstML, distance, pmWorld);
+		//while there is a node in consideration
+	}
+	
 	private void propagateMap(List<MapLocation> lstML, short distance, PlanetMap pmWorld){
 		while(!lstML.isEmpty()){
 			//ready a new lst for new nodes
@@ -59,7 +74,21 @@ public class MapSurface {
 			for(MapLocation ml: lstML){
 				//if there is nothing written at this node
 				//(written this way so that excess nodes do not produce children)				
-				if(Surface[ml.getX()][ml.getY()] == 0){
+				if((distance > 0 && Surface[ml.getX()][ml.getY()] < 1)){
+					//set the distance at this node
+					Surface[ml.getX()][ml.getY()] = distance;
+					//for each adjacent node
+					for(Direction d: Direction.values()){						
+						MapLocation newML = ml.add(d);
+						//if the node exists and has no text and is pathable
+						if(pmWorld.onMap(newML) && Surface[newML.getX()][newML.getY()] < 1 && pmWorld.isPassableTerrainAt(newML) == 1){
+							//record it
+							lstNextML.add(newML);
+						}
+					}	
+				}
+				//block is for negative distances
+				else if(Surface[ml.getX()][ml.getY()] == 0){
 					//set the distance at this node
 					Surface[ml.getX()][ml.getY()] = distance;
 					//for each adjacent node
